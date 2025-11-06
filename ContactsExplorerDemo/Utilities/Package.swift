9fcdb459c23dff29a46eba6e7843d38a98f19437
Eyal Katz <eyalkatz2403@gmail.com>
2025-11-06 07:37:25 +0200

    Created Utilities local SPM

---
+++ ContactsExplorerDemo/Utilities/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Utilities",
+    platforms: [.iOS(.v17), .watchOS(.v6)],
+    products: [
+        .library(
+            name: "Utilities",
+            targets: ["Utilities"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "Utilities"
+        ),
+        .testTarget(
+            name: "UtilitiesTests",
+            dependencies: ["Utilities"]
+        ),
+    ]
+)
