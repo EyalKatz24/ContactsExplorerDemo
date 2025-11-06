875539f6be76c51ae0a5d05cf97573ebeb26207a
Eyal Katz <eyalkatz2403@gmail.com>
2025-11-06 09:21:59 +0200

    Created contacts navigator

---
+++ ContactsExplorerDemo/Presentation/Sources/Navigators/Contacts/ContactsNavigator.swift
@@ -0,0 +1,59 @@
+//  
+//  ContactsNavigator.swift
+//  Presentation
+//
+//  Created by Eyal Katz on 06/11/2025.
+//
+
+import ComposableArchitecture
+import Contacts
+
+@Reducer
+public struct ContactsNavigator {
+    
+    @ObservableState
+    public struct State: Equatable {
+        var root: ContactsStore.State
+        var path = StackState<Path.State>()
+        
+        @Presents var destination: Destination.State?
+        
+        public init() {
+            self.root = .init()
+        }
+    }
+    
+    public enum Action {
+        case root(ContactsStore.Action)
+        case path(StackAction<Path.State, Path.Action>)
+        case destination(PresentationAction<Destination.Action>)
+    }
+    
+    public init() { }
+    
+    public var body: some ReducerOf<Self> {
+        Scope(state: \.root, action: \.root, child: ContactsStore.init)
+        
+        Reduce { state, action in
+            switch action {
+            case .root, .path, .destination:
+                return .none
+            }
+        }
+        .forEach(\.path, action: \.path)
+        .ifLet(\.$destination, action: \.destination)
+    }
+}
+
+extension ContactsNavigator {
+    
+    @Reducer(state: .equatable, action: .equatable)
+    public enum Path {
+
+    }
+    
+    @Reducer(state: .equatable)
+    public enum Destination {
+        
+    }
+}
---
+++ ContactsExplorerDemo/Presentation/Sources/Navigators/Contacts/ContactsNavigatorView.swift
@@ -0,0 +1,49 @@
+//  
+//  ContactsNavigatorView.swift
+//  Presentation
+//
+//  Created by Eyal Katz on 06/11/2025.
+//
+
+import SwiftUI
+import ComposableArchitecture
+import Contacts
+import DesignSystem
+
+public extension ContactsNavigator {
+    
+    struct ContentView: View {
+        
+        @Bindable public var store: StoreOf<ContactsNavigator>
+        
+        public init(store: StoreOf<ContactsNavigator>) {
+            self.store = store
+        }
+        
+        public var body: some View {
+            NavigationStack(
+                path: $store.scope(state: \.path, action: \.path),
+                root: {
+                    ContactsView(store: store.scope(state: \.root, action: \.root))
+                },
+                destination: { store in
+                    WithPerceptionTracking {
+                        switch store.case {
+                            // Remove if you don't have any other module in this navigator
+                        }
+                    }
+                }
+            )
+            .fullScreenCover(
+                item: $store.scope(state: \.destination, action: \.destination),
+                content: { store in
+                    WithPerceptionTracking {
+                        switch store.case {
+                            // Remove if you don't need any presentation
+                        }
+                    }
+                }
+            )
+        }
+    }
+}
