701c6d72712e498807c9eb544f4d3eb3f64feeda
Eyal Katz <eyalkatz2403@gmail.com>
2025-11-06 09:43:30 +0200

    Create Localization.swift

---
+++ ContactsExplorerDemo/Presentation/Sources/InternalUtilities/Localization/Localization.swift
@@ -0,0 +1,14 @@
+//
+//  Localization.swift
+//  Presentation
+//
+//  Created by Eyal Katz on 06/11/2025.
+//
+
+import Foundation
+import Localized
+
+@Localized(keyFormat: .lowerSnakeCase)
+public enum Localization: Equatable {
+    case email
+}
