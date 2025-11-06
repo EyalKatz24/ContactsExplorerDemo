b6b42ab48cdb7e5b1811e81943d48b4d70144c02
Eyal Katz <eyalkatz2403@gmail.com>
2025-11-06 19:30:39 +0200

    Create ContactDetailsView+Details.swift

---
+++ ContactsExplorerDemo/Presentation/Sources/Features/ContactDetails/Subviews/ContactDetailsView+Details.swift
@@ -0,0 +1,47 @@
+//
+//  ContactDetailsView+Details.swift
+//  Presentation
+//
+//  Created by Eyal Katz on 06/11/2025.
+//
+
+import SwiftUI
+import ComposableArchitecture
+import DesignSystem
+import Models
+
+extension ContactDetailsView {
+    
+    @ViewBuilder
+    private func detailsView() -> some View {
+        VStack(spacing: 20) {
+            DetailsSection(title: .phoneNumbers) {
+                ForEach(store.contact.phoneNumbers, id: \.self) { phoneNumber in
+                    DetailRowView(from: phoneNumber)
+                        .padding(.top, 8)
+                }
+            }
+            
+            if let birthday = store.contact.birthday {
+                DetailsSection(title: .birthday) {
+                    DetailRowView(
+                        title: .localized(.birthday),
+                        value: birthday.formatted(date: .long, time: .omitted),
+                        imageSystemName: "gift.fill"
+                    )
+                }
+            }
+            
+            if let emails = store.contact.emails, !emails.filter({ $0.address.isNotEmpty }).isEmpty {
+                DetailsSection(title: .emails) {
+                    ForEach(emails, id: \.self) { email in
+                        if email.address.isNotEmpty {
+                            DetailRowView(from: email)
+                                .padding(.top, 8)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
