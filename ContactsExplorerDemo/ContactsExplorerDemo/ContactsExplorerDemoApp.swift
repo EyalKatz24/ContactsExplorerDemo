971e21642e79e51f9f8ec5e1b6570fb4bf98d739
Eyal Katz <eyalkatz2403@gmail.com>
2025-11-06 07:24:47 +0200

    Created new project

---
+++ ContactsExplorerDemo/ContactsExplorerDemo/ContactsExplorerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsExplorerDemoApp.swift
+//  ContactsExplorerDemo
+//
+//  Created by Eyal Katz on 06/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct ContactsExplorerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
