280807ff411db8aef7523231753d4d1e2e8ca1cf
Eyal Katz <eyalkatz2403@gmail.com>
2025-11-06 08:02:59 +0200

    Update Services.swift

--- ContactsExplorerDemo/Data/Sources/Services/Services.swift
+++ ContactsExplorerDemo/Data/Sources/Services/Services.swift
@@ -5,3 +5,19 @@
 //  Created by Eyal Katz on 06/11/2025.
 //
 
+import Dependencies
+
+public struct Services {
+    
+}
+
+extension Services: DependencyKey {
+    public static let liveValue = Services()
+    public static let testValue = Services()
+}
+
+public extension DependencyValues {
+    var services: Services {
+        get { self[Services.self] }
+    }
+}
