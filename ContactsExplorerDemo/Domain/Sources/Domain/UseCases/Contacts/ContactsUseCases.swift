c347514531e2cee475c767859193595351e451fc
Eyal Katz <eyalkatz2403@gmail.com>
2025-11-06 09:05:31 +0200

    Update ContactsUseCases.swift

--- ContactsExplorerDemo/Domain/Sources/Domain/UseCases/Contacts/ContactsUseCases.swift
+++ ContactsExplorerDemo/Domain/Sources/Domain/UseCases/Contacts/ContactsUseCases.swift
@@ -9,11 +9,31 @@ import Dependencies
 import Models
 
 public struct ContactsUseCases {
-    
+    public var all: () -> [Contact]
+    public var retrieveAll: () async -> Void
+    public var requestContactsAuthorization: () async -> Bool
+    public var authorization: () -> ContactsAuthorization
 }
 
 extension ContactsUseCases: DependencyKey {
-    public static let liveValue = ContactsUseCases()
+    public static let liveValue = ContactsUseCases(
+        all: {
+            @Dependency(\.getAllContacts) var getAllContacts
+            return getAllContacts()
+        },
+        retrieveAll: {
+            @Dependency(\.retrieveContacts) var retrieveAllContacts
+            return await retrieveAllContacts()
+        },
+        requestContactsAuthorization: {
+            @Dependency(\.requestContactsAuthorization) var requestContactsAuthorization
+            return await requestContactsAuthorization()
+        },
+        authorization: {
+            @Dependency(\.getContactsAuthorization) var getContactsAuthorization
+            return getContactsAuthorization()
+        }
+    )
 }
 
 public extension DependencyValues {
