7add5b6e553280cf22963b9d9a1402c8e86918d3
Eyal Katz <eyalkatz2403@gmail.com>
2025-11-06 23:51:55 +0200

    Create ContactDetailsView+LabelButton.swift

---
+++ ContactsExplorerDemo/Presentation/Sources/Features/ContactDetails/Subviews/ContactDetailsView+LabelButton.swift
@@ -0,0 +1,35 @@
+//
+//  ContactDetailsView+LabelButton.swift
+//  Presentation
+//
+//  Created by Eyal Katz on 06/11/2025.
+//
+
+import SwiftUI
+import DesignSystem
+import Models
+
+extension ContactDetailsView {
+    
+    @ViewBuilder
+    func labelButton() -> some View {
+        Button {
+            send(.onContactFavoriteStatusTap)
+        } label: {
+            HStack {
+                Text(store.contact.isFavorite ? .markAsFavorite : .markedAsFavorite)
+                    .font(.body)
+                    .fontWeight(.heavy)
+                    .foregroundStyle(.primary)
+                    .tint(.primary)
+                    .contentTransition(.numericText())
+                
+                Image(systemName: store.contact.isFavorite ? "star.fill" : "star")
+                    .foregroundStyle(.yellow)
+                    .font(.title2)
+                    .contentTransition(.symbolEffect(.replace.offUp, options: .speed(0.7)))
+            }
+        }
+        .buttonStyle(.label)
+    }
+}
