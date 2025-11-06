4018c633c0979fdfa5ea1d3d723c71b726c180d8
Eyal Katz <eyalkatz2403@gmail.com>
2025-11-06 23:49:02 +0200

    Update CapsuleButtonStyle.swift

--- ContactsExplorerDemo/Presentation/Sources/DesignSystem/Styles/Button/CapsuleButtonStyle.swift
+++ ContactsExplorerDemo/Presentation/Sources/DesignSystem/Styles/Button/CapsuleButtonStyle.swift
@@ -9,17 +9,33 @@ import SwiftUI
 
 public struct CapsuleButtonStyle: ButtonStyle {
     
-    // TODO: liquid glass support
+    @Environment(\.isEnabled) private var isEnabled: Bool
+    
     public func makeBody(configuration: Configuration) -> some View {
-        configuration.label
-            .padding(.horizontal, 16)
-            .padding(.vertical, 10)
-            .background(
-                Capsule()
-                    .fill(Color.accentColor)
-            )
-            .foregroundColor(.white)
-            .opacity(configuration.isPressed ? 0.6 : 1.0)
+        if #available(iOS 26.0, *) {
+            configuration.label
+                .padding(.horizontal, 16)
+                .padding(.vertical, 10)
+                .background(
+                    Capsule()
+                        .fill(Color.accentColor)
+                )
+                .foregroundColor(.white)
+                .opacity(configuration.isPressed ? 0.8 : 1.0)
+                .glassEffect(.regular.interactive(isEnabled), in: .capsule)
+                .clipShape(.capsule)
+        } else {
+            configuration.label
+                .padding(.horizontal, 16)
+                .padding(.vertical, 10)
+                .background(
+                    Capsule()
+                        .fill(Color.accentColor)
+                )
+                .foregroundColor(.white)
+                .opacity(configuration.isPressed ? 0.6 : 1.0)
+                .clipShape(.capsule)
+        }
     }
 }
 
