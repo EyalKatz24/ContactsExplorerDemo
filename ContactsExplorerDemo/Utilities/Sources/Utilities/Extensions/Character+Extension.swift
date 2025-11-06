5d59ffa964db69ed145bab600e87ab3ca4a4e33f
Eyal Katz <eyalkatz2403@gmail.com>
2025-11-06 08:48:58 +0200

    Created String + Character extensions (Utilities)

---
+++ ContactsExplorerDemo/Utilities/Sources/Utilities/Extensions/Character+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  Character+Extension.swift
+//  Utilities
+//
+//  Created by Eyal Katz on 06/11/2025.
+//
+
+import Foundation
+
+public extension Character {
+    
+    static let whitespace: Character = " "
+    static let slash: Character = "/"
+}
