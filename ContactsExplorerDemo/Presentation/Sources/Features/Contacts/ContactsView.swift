e6308e577b19ada5b6793cde4d35883932ee5404
Eyal Katz <eyalkatz2403@gmail.com>
2025-11-09 00:47:44 +0200

    Update ContactsView.swift
    Scroll indicators should be presented on loaded state

--- ContactsExplorerDemo/Presentation/Sources/Features/Contacts/ContactsView.swift
+++ ContactsExplorerDemo/Presentation/Sources/Features/Contacts/ContactsView.swift
@@ -21,7 +21,7 @@ public struct ContactsView: View {
     
     public var body: some View {
         GeometryReader { geometry in
-            ScrollView(showsIndicators: false) {
+            ScrollView(showsIndicators: store.viewState == .loaded) {
                 LazyVStack(spacing: 0) {
                     switch store.viewState {
                     case .loading:
